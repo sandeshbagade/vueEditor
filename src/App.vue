58c534e412f8e5152238bd64dd91d6263f420394
Sandesh Bagade <sandeshbagade25@gmail.com>
2019-11-25 11:38:38 +0530

    non editable added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
  <div>
    <Child />
-    <Child />
   </div>
 </template>
 
--- src/components/Child.vue
+++ src/components/Child.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    <vue-editor v-model="content"></vue-editor>
+     <h1>Primary editor</h1>
+    <vue-editor v-model="content" :editorToolbar="customToolbar"></vue-editor>
+    <h1>Non-Editable</h1>
+      <div v-html="content" style="min-height:300px;border:1px solid black;"></div>
+    <h2>Editable (2 ways)</h2>  
+     <vue-editor id="editor2" v-model="content"></vue-editor>
   </div>
 </template>
  
@@ -14,6 +19,7 @@ export default {
  
   data() {
     return {
+      customToolbar: [["bold", "italic", "underline"], [{ list: "ordered" }, { list: "bullet" }], ["image", "code-block"]],
       content: "<h1>Enthire</h1>"
     };
   }
