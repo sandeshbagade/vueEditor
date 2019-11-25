8b0337133b1ea783bf783d7e77310eac482ef763
Sandesh Bagade <sandeshbagade25@gmail.com>
2019-11-25 10:41:26 +0530

    initial set up

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <Child />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Child from './components/Child.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Child
   }
 }
 </script>
